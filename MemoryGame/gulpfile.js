e8ba7df05d77a0f8be37d65eed08b7b361f0577d
sofiaH2 <sherrerah1@ucenfotec.ac.cr>
2023-07-23 01:13:20 -0500

    add gulpfile.js

---
+++ MemoryGame/gulpfile.js
@@ -0,0 +1,9 @@
+const rollup = require('rollup');
+
+exports.bundle = ()=>{
+    console.log('GULP- Running bundle function');
+};
+
+exports.play = ()=>{
+    console.log('GULP- Running play function');
+};
