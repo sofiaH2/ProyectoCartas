7988135b1d1ad3044dc263542d8a54dad79f4cd3
sofiaH2 <sherrerah1@ucenfotec.ac.cr>
2023-08-16 17:23:43 -0500

    add changes

--- MemoryGame/js/controllers/credits/creditsView.js
+++ MemoryGame/js/controllers/credits/creditsView.js
@@ -7,25 +7,21 @@ export class CreditsView extends ControllerView {
         this.elementsContainer.parentElement.style.width = '100%';
 
 
-        var text = document.createElement('p')
-        this.elementsContainer.appendChild(text);
-        text.innerHTML = 'Echo por:  Sofia Herrera Herrera';
-        
-        var text = document.createElement('p')
-        this.elementsContainer.appendChild(text);
-        text.innerHTML = 'Tipo:Juego de Memoria';
-
-        var text = document.createElement('p')
-        this.elementsContainer.appendChild(text);
-        text.innerHTML = 'Clase:Programacion Web Orientada a Objetos';
-
-        var text = document.createElement('p')
-        this.elementsContainer.appendChild(text);
-        text.innerHTML = 'Universidad:Estudiante de Cenfotec';
-
-        var text = document.createElement('p')
-        this.elementsContainer.appendChild(text);
-        text.innerHTML = 'Agosto 2023';
+        var text = document.createElement('p');
+this.elementsContainer.appendChild(text);
+text.innerHTML = 'Créditos del Juego "Memory Game"<br>' +
+    'Desarrollado por: Sofía Herrera<br>' +
+    'Equipo de Desarrollo:<br>' +
+    '- Diseño de Juego: Sofía Herrera<br>' +
+    
+   
+'- Programación: Sofía Herrera<br>' +
+    '**Agradecimientos Especiales:** Queremos expresar nuestro agradecimiento a todas<br> las personas que han contribuido con su apoyo y comentarios<br> valiosos durante el desarrollo de "Memory Game".<br> Su retroalimentación ha sido fundamental para hacer de <br>este juego una experiencia entretenida y desafiante.<br>' +
+    
+   
+'**Fecha de Creación:** 16 de Agosto de 2023<br>' +
+    '**Contacto:** Para consultas, comentarios o asistencia técnica,<br> por favor contactar a Sofía Herrera a través de<br> <a href="mailto:sofia.herrera@gmail.com">sofia.herrera@gmail.com</a>.<br>' +
+    '**Derechos de Autor:** "Memory Game" y todos los elementos visuales<br> y sonoros asociados son propiedad exclusiva de Sofía Herrera,<br> bajo los derechos de autor correspondientes.';
 
 
     }
